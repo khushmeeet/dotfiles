78bae64434ca83ed312ab2a6c4d8cefec3c4f6be
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-02-05 19:00:59 -0500

    Change wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
     color_scheme = 'ayu',
-    font = wezterm.font('SF Mono', { weight = 'Bold' }),
+    font = wezterm.font('Berkeley Mono Trial', { weight = 'Bold' }),
     keys = {
         {key="w", mods="SUPER|SHIFT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
         {key="-", mods="SUPER|ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
