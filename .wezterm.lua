32567f364bf8d8ec33cecbb36e86e2948f6ad193
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-11-19 17:31:30 -0800

    Adding updated dotfiles

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm';
+
+return {
+    color_scheme = 'Argonaut',
+    font = wezterm.font('Berkeley Mono', { weight = 'Bold' }),
+    font_size = 13,
+    keys = {
+        {key="w", mods="SUPER|SHIFT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+        {key="-", mods="SUPER|ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="\\", mods="SUPER|ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}}
+    }
+}
